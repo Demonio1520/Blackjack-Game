736ba4c424a4c3d95228d69b5c54a389dfb5a8a8
Henry <henry.vega2005@gmail.com>
2024-03-20 18:28:48 -0400

    firts commit

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,12 +1,61 @@
-import '../css/functions.css';
-import webpackImg from '../assets/img/webpack-logo.png';
+// Shuffle Arrays
 
-export const body = (name) => {
-    const h1 = document.createElement('h1');
-    const img = document.createElement('img');
-
-    h1.innerText = `Hola ${name}.`;
-    img.src = webpackImg;
-    document.body.append(h1);
-    document.body.append(img);
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]]
+    }
 }
+
+// Create Number Of Players
+const numberPlayers = (numberPlayers) => {
+    for (let i = 1; i <= numberPlayers; i++) {
+        const players = document.querySelectorAll('#players'),
+              div = document.createElement('div'),
+              div2 = document.createElement('div'),
+              h2 = document.createElement('h2'),
+              span = document.createElement('span');
+
+        if (i < numberPlayers) {
+            players[0].appendChild(div);
+            div.classList.add('col-4');
+            div.appendChild(h2);
+            div.appendChild(div2);
+            (i == 1) ? h2.textContent = 'You - ' : h2.textContent = `Player ${i} - `;
+            h2.append(span);
+            div2.id = 'player-cards'
+        } else {
+            players[1].appendChild(div);
+            div.classList.add('col-4');
+            div.appendChild(h2);
+            div.appendChild(div2);
+            h2.textContent = 'Dealer - ';
+            h2.append(span);
+            div2.id = 'player-cards'
+        }
+    }
+}
+
+// Face Down Card
+const addDownCards = () => {
+    downCardValue = getCard();
+
+    // Create Card
+    const imgCard = document.createElement('img');
+    imgCard.classList.add('cards');
+    imgCard.src = `/img/grey_back.png`;
+    imgCard.alt = "Card Face-Down";
+    computerCards.append(imgCard);
+}
+
+// ¡Blackjack!
+const Blackjack = () => {
+    if ((cardCounter[0][0].charAt(0)) == 'A' && isNaN (cardCounter[0][1].charAt(0)) || (cardCounter[0][1].charAt(0)) == 'A' && isNaN (cardCounter[0][0].charAt(0))) {
+        btnGet.disabled = true;
+        setTimeout(() => {
+            alert('¡Blackjack!');          
+        }, 1000);
+    }
+}
+
+// As Value
