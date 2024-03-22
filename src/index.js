0d124db81610d346a2cb720baaae4c80c0a0d2a1
Henry <henry.vega2005@gmail.com>
2024-03-22 18:56:51 -0400

    ultimate commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import {Game,Card} from './js/functions.js';
 
 export let players = 1, turn = 0;
 export const startGame = new Game(players), newCard = new Card;
-console.log(newCard.deck);
 
 // HTML Selectors
 export const addCard = document.querySelectorAll('#player-cards'),
